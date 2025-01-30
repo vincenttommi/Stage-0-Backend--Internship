405145812aeb7e3ad395951ad9fcb695ec8c650f
vincenttommi <vincenttommi@gmail.com>
2025-01-30 16:27:52 +0300

    code completed

---
+++ publication/core/views.py
@@ -0,0 +1,11 @@
+from django.utils.timezone import now
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class PublicationAPI(APIView):
+    def get(self, request):  # Include 'self' as the first argument
+        return Response({
+            "email": "vincenttommikorir@gmail.com",
+            "current_datetime": now().isoformat(), 
+            "github_url": "https://github.com/vincenttommi/Stage-0-Backend--Internship"
+        })
