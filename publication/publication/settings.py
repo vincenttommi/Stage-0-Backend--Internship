dfb4bb0f55006548a16b0a106810e2b625c97200
vincenttommi <vincenttommi@gmail.com>
2025-01-31 20:47:02 +0300

    code

--- publication/publication/settings.py
+++ publication/publication/settings.py
@@ -9,12 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i%s-v#%^z@ozicd9f@%o%c68_-x5%*l#@$%t)9*k+9e0id4u)w'
+# SECRET_KEY = 'django-insecure-i%s-v#%^z@ozicd9f@%o%c68_-x5%*l#@$%t)9*k+9e0id4u)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -67,7 +65,13 @@ WSGI_APPLICATION = 'publication.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -108,18 +112,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR/ 'static'
-]
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage", 
-    },
-}
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+import django_heroku
+django_heroku.settings(locals())
--- publication/publication/wsgi.py
+++ publication/publication/wsgi.py
@@ -1,7 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'publication.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "publication.settings")
 
 application = get_wsgi_application()
